8f595d961b2184533cf587c9e0fb619650d69e5c
Alyamama6 <alyadvweb@gmail.com>
2026-05-16 22:43:16 +0400

    Update guidereg.dart

--- lib/guidereg.dart
+++ lib/guidereg.dart
@@ -11,7 +11,6 @@ import 'package:path/path.dart' as p;
 
 class GuideRegistrationScreen extends StatefulWidget {
   const GuideRegistrationScreen({super.key});
-
   @override
   State<GuideRegistrationScreen> createState() =>
       _GuideRegistrationScreenState();
