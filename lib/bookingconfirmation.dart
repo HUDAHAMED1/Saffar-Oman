a121b01d1767a6bba166f1bd070d8c82f94b2d21
Alyamama6 <alyadvweb@gmail.com>
2026-05-16 22:43:48 +0400

    Update bookingconfirmation.dart

--- lib/bookingconfirmation.dart
+++ lib/bookingconfirmation.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class BookingConfirmation extends StatelessWidget {
   const BookingConfirmation({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
